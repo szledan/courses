ef138f6d032e4fbcf06fd247df80a4b87e271595
Áron, Sánta <aaannorst@gmail.com>
2021-03-18 19:17:01 +0200

    ready to read lines of sample file

---
+++ 02-prime-numbers/solutions/aaannorst/main.py
@@ -0,0 +1,14 @@
+'''decide if the numbers on the list are primes'''
+
+# open text wile with numbers
+f = open('./basic-inputs.txt', 'r')
+
+sample = f.readlines()
+for line in  sample:
+	print(line)
+# f.close()
+
+# read and sort the numbers from the file 
+
+
+
